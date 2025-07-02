2ffd8a6c1fa414d6eaed04be331b2c4197976816
Bimochan Shrestha <bimoai857@gmail.com>
2025-07-02 17:43:50 +0545

    Adjusted the score threshold

--- services/chatbot.py
+++ services/chatbot.py
@@ -33,7 +33,7 @@ vector_store = Qdrant(
 retriever = vector_store.as_retriever(
     search_type="similarity_score_threshold",
     search_kwargs={
-        "score_threshold": 0.5,
+        "score_threshold": 0.35,
         "k": 5
     }
 )
